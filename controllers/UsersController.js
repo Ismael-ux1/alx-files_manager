10cc6d81c4fb859b4b57df652c1724b3f25c43ae
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-13 12:11:37 -0700

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
-const redisClient = require('../redisClient');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -35,21 +35,6 @@ class UsersController {
       console.error('Error creating user:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
-  static async getMe(req, res) {
-    const token = req.headers['x-token'];
-    const userId = await redisClient.get(`auth_${token}`);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const user = await dbClient.db.collection('users').findOne({ _id: userId });
-    if (!user) {
-      return res.status(404).json({ error: 'Not found' });
-    }
-
-    return res.status(200).json({ email: user.email, id: user._id });
   }
 }
 
